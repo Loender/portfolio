84dd0d68587a0f215492239b6e2042d2f7bdd623
Loender <loendera@gmail.com>
2025-07-04 06:41:10 +0300

    Revert "Merge branch 'main' of https://github.com/Loender/portfolio"
    This reverts commit 36e8d3a31488543c50ba30b66146ecb3967ff520, reversing
    changes made to 0eff152bdd94c8e73e916c8d50a7efc717deeed0.

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,11 @@
 import type { NextConfig } from "next";
 
-<<<<<<< HEAD
 const isProd = process.env.NODE_ENV = 'production';
 
-=======
-const isProd = process.env.NODE_ENV === 'production';
->>>>>>> 3bc9f2596629faf17c758229a31412c9148a8e49
 const nextConfig: NextConfig = {
-  basePath: isProd ? '/portfolio' : '',
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   output: 'export',
   images:{
     unoptimized: true,
