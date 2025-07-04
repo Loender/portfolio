c1ab283389143a7088b5f326c7c03044836c29b9
Oleksii <44601921+Loender@users.noreply.github.com>
2025-07-04 06:18:27 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,8 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV = 'production';
 const nextConfig: NextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  basePath: isProd ? '/portfolio' : '',
   output: 'export',
   images:{
     unoptimized: true,
